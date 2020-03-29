81dd257ccf781f14bca4100a4253416556fd2aa5
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-29 10:29:59 +0200

    ProductHandlerTest coverage

--- src/main/java/com/dgf/shopproduct/rest/handler/ProductHandler.java
+++ src/main/java/com/dgf/shopproduct/rest/handler/ProductHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import static com.dgf.shopproduct.Constants.PRODUCTS;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
--- src/test/java/com/dgf/shopproduct/rest/handler/ProductHandlerTest.java
+++ src/test/java/com/dgf/shopproduct/rest/handler/ProductHandlerTest.java
@@ -11,13 +11,17 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
 @AutoConfigureDataMongo
 //@DataMongoTest
@@ -47,9 +51,11 @@ public class ProductHandlerTest extends BaseHandlerTest {
 
     @Test
     public void find() {
-        MockServerWebExchange exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/product/find/aa"));
-        Mockito.when(service.findAll()).thenReturn(Flux.fromIterable(new ArrayList<>()));
-        StepVerifier.create(handler.list(getDefaultServerReq(exchange)).flatMap(response -> {
+        MockServerWebExchange exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/product/find"));
+        Map<String, String> pathVariables = Collections.singletonMap("name", "aa");
+        exchange.getAttributes().put(RouterFunctions.URI_TEMPLATE_VARIABLES_ATTRIBUTE, pathVariables);
+        Mockito.when(service.findByNameIgnoreCaseContaining(any())).thenReturn(Flux.fromIterable(new ArrayList<>()));
+        StepVerifier.create(handler.find(getDefaultServerReq(exchange)).flatMap(response -> {
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK);
             return response.writeTo(exchange, context);
         })).expectComplete().verify();
